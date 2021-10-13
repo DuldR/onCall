0c1b0d43118b0c24279fee98132f45e64e4950c2
Garrett <gnjulaka@gmail.com>
2021-10-13 15:59:16 -0500

    Month tests ready.

--- spec/models/month_spec.rb
+++ spec/models/month_spec.rb
@@ -13,5 +13,28 @@
 require 'rails_helper'
 
 RSpec.describe Month, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:month_num) }
+
+
+  end
+
+  describe "associations" do
+
+    it { should have_many(:shifts) }
+    
+  end
+
+  describe "class methods" do
+
+    describe "#month_setup" do
+      it "should take the month number and auto fill name and start/stop days" do
+        pending
+      end
+    end
+    
+    
+  end
 end
