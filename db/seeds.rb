cba62454b19fa3987193d698d9f60376ecc49609
Garrett <gnjulaka@gmail.com>
2021-10-17 22:15:43 -0500

    Seeds added. Everything working.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,7 @@ months.each do |month|
     seed_month.month_num = month
     seed_month.save!
 end
+
+User.create(name: "TestMan")
+Shift.create(shift_start: Date.new(2021,1,1), shift_end: Date.new(2021,1,2), user_id: 1, month_id: 1)
+Swap.create(user_id: 1, shift_id: 1, target_id: 2, target_shift: 2)
