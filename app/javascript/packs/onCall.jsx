817bd0d663cf525699c0154e17abbef6f3ee5366
Garrett <gnjulaka@gmail.com>
2021-10-19 21:07:37 -0500

    OK, reset webpack.

--- app/javascript/packs/onCall.jsx
+++ app/javascript/packs/onCall.jsx
@@ -4,10 +4,6 @@ import Rails from '@rails/ujs'
 import { Root } from '../components/root'
 import configureStore from '../store/store'
 
-// import { fetchOneKanji } from '../actions/kanji_actions'
-// import * as APIUtil from '../util/kanji_util'
-
-// Actions
 
 
 
@@ -18,5 +14,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root")
   ReactDOM.render( <Root store={store}/>, root)
 
-  window.store = store;
 })
